a64e86fbf135878ed22bcfa33da03e0c7df86677
Martin Finda <martin.finda@gmail.con>
2024-09-12 07:12:00 +0200

    porovnani

--- sketch_aug16a/sketch_aug16a.ino
+++ sketch_aug16a/sketch_aug16a.ino
@@ -1,25 +1,107 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-//char pole [16][2];
-int pozice_x=0;
-int pozice_y=0;
+int sensorReading = 0;
+uint8_t pozice_x=0;
+uint8_t pozice_y=0;
 char stav='c';
+
+byte heart[8] = {
+  0b00000,
+  0b01010,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00000
+};
+
+byte smiley[8] = {
+  0b00000,
+  0b00000,
+  0b01010,
+  0b00000,
+  0b00000,
+  0b10001,
+  0b01110,
+  0b00000
+};
+
+byte frownie[8] = {
+  0b00000,
+  0b00000,
+  0b01010,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b01110,
+  0b10001
+};
+
+byte armsDown[8] = {
+  0b00100,
+  0b01010,
+  0b00100,
+  0b00100,
+  0b01110,
+  0b10101,
+  0b00100,
+  0b01010
+};
+
+byte armsUp[8] = {
+  0b00100,
+  0b01010,
+  0b00100,
+  0b10101,
+  0b01110,
+  0b00100,
+  0b00100,
+  0b01010
+};
 
 void setup() {
   lcd.begin(16, 2);
+    lcd.createChar(1, heart);
+    lcd.createChar(2, smiley);
 }
 
 void loop() {
-  int sensorReading = analogRead(A0);
-  
-  
-  
+    sensorReading = analogRead(A0);
     
-  /*if(sensorReading>0&&sensorReading<100){pozice_x++;} else if(sensorReading>2500&&sensorReading<2600){pozice_y--;} else if(sensorReading>4000&&sensorReading<4200){pozice_x--;} else if(sensorReading>6300&&sensorReading<6500){stav='r';} else if(sensorReading>9800&&sensorReading<10000){pozice_x++;}*/
-    if(sensorReading>0&&sensorReading<100){pozice_x++;}
-    if(pozice_x>16)pozice_x=0;
- lcd.setCursor(pozice_x,pozice_y);
-    lcd.print(sensorReading);
+    if(sensorReading<100) {
+        pozice_x++;
+    }
+    else if(sensorReading<2500) {
+        lcd.setCursor(8, 1);
+        lcd.print(sensorReading);
+    } else if(sensorReading<2600) {
+        pozice_y--;
+    } else if(sensorReading<4200) {
+        pozice_x--;
+    } else if(sensorReading<6500) {
+        stav='r';
+    } else if(sensorReading<10000) {
+        pozice_x++;
+    }
+    
+    if(pozice_x > 16) {
+        pozice_x = 0;
+    }
+    if(pozice_y > 1) {
+        pozice_y = 0;
+    }
+    
+    lcd.setCursor(0, 0);
+    lcd.print(pozice_x);
+    lcd.setCursor(0, 1);
+    lcd.print(pozice_y);
+    /*if(sensorReading == 0U) {
+        lcd.write(2);
+    }
+    else {
+        lcd.write(1);
+    }*/
     delay(100);
 }
